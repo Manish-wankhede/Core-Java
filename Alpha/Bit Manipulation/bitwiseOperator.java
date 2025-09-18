1db2d2af46302c389be1ea9a973b6940139bf9c0
Manish Wankhede <132348233+Manish-wankhede@users.noreply.github.com>
2025-09-18 16:42:08 +0530

    Add files via upload

---
+++ Alpha/Bit Manipulation/bitwiseOperator.java
@@ -0,0 +1,9 @@
+public class bitwiseOperator {
+    public static void main(String[] args) {
+        // System.out.println(7 & 8);
+        // System.out.println(5 | 6);
+        // System.out.println(~5);
+        System.out.println(5<<4);
+    }
+    
+}
